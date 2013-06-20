e3141effe9746a53b08dadcd85dac6bf3db7f8e7
Ben Rector <ben.rector@gmail.com>
2013-06-20 16:03:04 -0600

    Added List and Update

--- src/main/java/us/co/state/sos/licenserequest/LicenseRequestService.java
+++ src/main/java/us/co/state/sos/licenserequest/LicenseRequestService.java
@@ -1,21 +1,39 @@
 package us.co.state.sos.licenserequest;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public class LicenseRequestService {
-	
+
 	@PersistenceContext
 	private EntityManager entityManager;
-	
+
 	@Transactional
 	public String create(LicenseRequest licenseRequest) {
 		System.out.format("License for %s accepted\n", licenseRequest.getLastName());
 		entityManager.persist(licenseRequest);
 		return "Success";
 	}
+
+	public LicenseRequest load(Long id) {
+		LicenseRequest licenseRequest = entityManager.find(LicenseRequest.class, id);
+		if (licenseRequest != null)
+			System.out.format("Found License for %s accepted\n", licenseRequest.getLastName());
+		return licenseRequest;
+	}
+
+	public List<LicenseRequest> list() {
+		TypedQuery<LicenseRequest> query = entityManager.createQuery("SELECT a from LicenseRequest as a",
+				LicenseRequest.class);
+
+		return query.getResultList();
+	}
+
 }
